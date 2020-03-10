ca5b35684d5f658319e94364c37a8680cd2b937c
novg <novg.novg@gmail.com>
2020-03-10 23:06:46 +0700

    Add CompleteWindow

---
+++ CompleteWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using Microsoft.Win32;
+using RentailInvoice.Interfaces;
+
+namespace RentailInvoiceWPF
+{
+    /// <summary>
+    /// Interaction logic for Complete.xaml
+    /// </summary>
+    public partial class Complete : Window
+    {
+        public Complete()
+        {
+            InitializeComponent();
+        }
+
+        public IDataWriter DataWriter { get; set; }
+
+        private void buttonSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Файлы Excel|*.xlsx";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    DataWriter.Save(saveFileDialog.FileName);
+                    this.Close();
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка сохранения файла", MessageBoxButton.OK, MessageBoxImage.Error);
+                    this.Close();
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,8 +21,32 @@ namespace RentailInvoiceWPF
             TextFilePath.Text = droppedPaths[0];
             //
             IDataReader dataReader = new CalculateDataReader(TextFilePath.Text);
-            IDataWriter dataWriter = new Payment(dataReader.Read());
-            dataWriter.Write();
+            IDataWriter dataWriter;
+            try
+            {
+                dataWriter = new Payment(dataReader.Read());
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка чтения данных", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw;
+            }
+
+            try
+            {
+                dataWriter.Write();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка формирования данных", MessageBoxButton.OK, MessageBoxImage.Error);
+                throw;
+            }
+
+            Complete complete = new Complete
+            {
+                DataWriter = dataWriter
+            };
+            complete.ShowDialog();
         }
     }
 }
