1ed6ef534053b6d0a9059dfee13273319a86eba2
novg <novg.novg@gmail.com>
2020-03-10 21:38:47 +0700

    Add RentailInvoice reference

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using RentailInvoice.DataReader;
+using RentailInvoice.DataWriter;
+using RentailInvoice.Interfaces;
 
 namespace RentailInvoiceWPF
 {
@@ -29,6 +19,10 @@ namespace RentailInvoiceWPF
         {
             var droppedPaths = e.Data.GetData(DataFormats.FileDrop, true) as string[];
             TextFilePath.Text = droppedPaths[0];
+            //
+            IDataReader dataReader = new CalculateDataReader(TextFilePath.Text);
+            IDataWriter dataWriter = new Payment(dataReader.Read());
+            dataWriter.Write();
         }
     }
 }
